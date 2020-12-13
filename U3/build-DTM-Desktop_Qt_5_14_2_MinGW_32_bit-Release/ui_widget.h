05687f951d40b37259ffe773a88c2bccbf1add8b
breznicanova <breznicanova.adriana@gmail.com>
2020-12-13 15:02:57 +0100

    ui update

---
+++ U3/build-DTM-Desktop_Qt_5_14_2_MinGW_32_bit-Release/ui_widget.h
@@ -0,0 +1,268 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include <draw.h>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QHBoxLayout *horizontalLayout;
+    Draw *Canvas;
+    QFrame *line;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label_4;
+    QComboBox *comboBox_2;
+    QPushButton *pushButton_5;
+    QLabel *label_5;
+    QPushButton *pushButton;
+    QFrame *line_2;
+    QSpacerItem *verticalSpacer;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QLabel *label_3;
+    QLineEdit *lineEdit_3;
+    QPushButton *pushButton_7;
+    QLabel *Analyze;
+    QComboBox *comboBox;
+    QPushButton *pushButton_2;
+    QSpacerItem *verticalSpacer_4;
+    QFrame *line_3;
+    QPushButton *pushButton_11;
+    QPushButton *pushButton_12;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QSpacerItem *verticalSpacer_6;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(1062, 681);
+        horizontalLayout = new QHBoxLayout(Widget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        Canvas = new Draw(Widget);
+        Canvas->setObjectName(QString::fromUtf8("Canvas"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(1);
+        sizePolicy.setVerticalStretch(1);
+        sizePolicy.setHeightForWidth(Canvas->sizePolicy().hasHeightForWidth());
+        Canvas->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(Canvas);
+
+        line = new QFrame(Widget);
+        line->setObjectName(QString::fromUtf8("line"));
+        line->setFrameShape(QFrame::VLine);
+        line->setFrameShadow(QFrame::Sunken);
+
+        horizontalLayout->addWidget(line);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        label_4 = new QLabel(Widget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        QFont font;
+        font.setBold(true);
+        font.setWeight(75);
+        label_4->setFont(font);
+
+        verticalLayout_3->addWidget(label_4);
+
+        comboBox_2 = new QComboBox(Widget);
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->setObjectName(QString::fromUtf8("comboBox_2"));
+
+        verticalLayout_3->addWidget(comboBox_2);
+
+        pushButton_5 = new QPushButton(Widget);
+        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
+
+        verticalLayout_3->addWidget(pushButton_5);
+
+        label_5 = new QLabel(Widget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setFont(font);
+
+        verticalLayout_3->addWidget(label_5);
+
+        pushButton = new QPushButton(Widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        verticalLayout_3->addWidget(pushButton);
+
+        line_2 = new QFrame(Widget);
+        line_2->setObjectName(QString::fromUtf8("line_2"));
+        line_2->setFrameShape(QFrame::HLine);
+        line_2->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_3->addWidget(line_2);
+
+        verticalSpacer = new QSpacerItem(20, 60, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer);
+
+        label = new QLabel(Widget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setFont(font);
+
+        verticalLayout_3->addWidget(label);
+
+        lineEdit = new QLineEdit(Widget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        verticalLayout_3->addWidget(lineEdit);
+
+        label_2 = new QLabel(Widget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setFont(font);
+
+        verticalLayout_3->addWidget(label_2);
+
+        lineEdit_2 = new QLineEdit(Widget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+
+        verticalLayout_3->addWidget(lineEdit_2);
+
+        label_3 = new QLabel(Widget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setFont(font);
+
+        verticalLayout_3->addWidget(label_3);
+
+        lineEdit_3 = new QLineEdit(Widget);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+
+        verticalLayout_3->addWidget(lineEdit_3);
+
+        pushButton_7 = new QPushButton(Widget);
+        pushButton_7->setObjectName(QString::fromUtf8("pushButton_7"));
+
+        verticalLayout_3->addWidget(pushButton_7);
+
+        Analyze = new QLabel(Widget);
+        Analyze->setObjectName(QString::fromUtf8("Analyze"));
+        Analyze->setFont(font);
+
+        verticalLayout_3->addWidget(Analyze);
+
+        comboBox = new QComboBox(Widget);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+
+        verticalLayout_3->addWidget(comboBox);
+
+        pushButton_2 = new QPushButton(Widget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        verticalLayout_3->addWidget(pushButton_2);
+
+        verticalSpacer_4 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer_4);
+
+        line_3 = new QFrame(Widget);
+        line_3->setObjectName(QString::fromUtf8("line_3"));
+        line_3->setFrameShape(QFrame::HLine);
+        line_3->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_3->addWidget(line_3);
+
+        pushButton_11 = new QPushButton(Widget);
+        pushButton_11->setObjectName(QString::fromUtf8("pushButton_11"));
+
+        verticalLayout_3->addWidget(pushButton_11);
+
+        pushButton_12 = new QPushButton(Widget);
+        pushButton_12->setObjectName(QString::fromUtf8("pushButton_12"));
+
+        verticalLayout_3->addWidget(pushButton_12);
+
+        pushButton_3 = new QPushButton(Widget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        verticalLayout_3->addWidget(pushButton_3);
+
+        pushButton_4 = new QPushButton(Widget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+
+        verticalLayout_3->addWidget(pushButton_4);
+
+        verticalSpacer_6 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer_6);
+
+
+        horizontalLayout->addLayout(verticalLayout_3);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "DTM", nullptr));
+        label_4->setText(QCoreApplication::translate("Widget", "Select terrain feature", nullptr));
+        comboBox_2->setItemText(0, QCoreApplication::translate("Widget", "Knoll", nullptr));
+        comboBox_2->setItemText(1, QCoreApplication::translate("Widget", "Valley", nullptr));
+        comboBox_2->setItemText(2, QCoreApplication::translate("Widget", "Spur", nullptr));
+        comboBox_2->setItemText(3, QCoreApplication::translate("Widget", "Ridge", nullptr));
+
+        pushButton_5->setText(QCoreApplication::translate("Widget", "Generate terrain", nullptr));
+        label_5->setText(QCoreApplication::translate("Widget", "Load from file", nullptr));
+        pushButton->setText(QCoreApplication::translate("Widget", "Load points", nullptr));
+        label->setText(QCoreApplication::translate("Widget", "Minimum height [m]", nullptr));
+        lineEdit->setText(QCoreApplication::translate("Widget", "0", nullptr));
+        label_2->setText(QCoreApplication::translate("Widget", "Maximum height [m]", nullptr));
+        lineEdit_2->setText(QCoreApplication::translate("Widget", "500", nullptr));
+        label_3->setText(QCoreApplication::translate("Widget", "Interval of contours [m]", nullptr));
+        lineEdit_3->setText(QCoreApplication::translate("Widget", "1", nullptr));
+        pushButton_7->setText(QCoreApplication::translate("Widget", "Contour lines", nullptr));
+        Analyze->setText(QCoreApplication::translate("Widget", "Select an analyze:", nullptr));
+        comboBox->setItemText(0, QCoreApplication::translate("Widget", "Slope", nullptr));
+        comboBox->setItemText(1, QCoreApplication::translate("Widget", "Aspect", nullptr));
+
+        pushButton_2->setText(QCoreApplication::translate("Widget", "Analyze DTM", nullptr));
+        pushButton_11->setText(QCoreApplication::translate("Widget", "Clear Points", nullptr));
+        pushButton_12->setText(QCoreApplication::translate("Widget", "Clear contours", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("Widget", "Clear analysis", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("Widget", "Clear all", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
